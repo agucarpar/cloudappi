e33839e5e8163771d3ebd78f03807c33994bfb51
root <root@DESKTOP-COVV5F4.localdomain>
2019-09-03 20:09:46 +0200

    probando

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const port = process.env.PORT || 8080
 const serverStatic = require('server-static')
 const path = require('path')
 
@@ -8,6 +9,5 @@ const app=express()
 
 app.use('/', serverStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 6000
 
 app.listen(port)
